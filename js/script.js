634a9fda01fbd043e4cbe57cf2cc26f75774abf4
soubhikdas <d.soubhik@hotmail.com>
2025-07-14 12:11:06 +0530

    up up date .. update ⬆️

--- js/script.js
+++ js/script.js
@@ -1,41 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var url_string = window.location.href;
-  var url = new URL(url_string);
-  var q = url.searchParams.get("f");
-  var m = url.searchParams.get("m");
-  var r = url.searchParams.get("r");
-  var tostr = url.searchParams.toString();
-  var fullurl = tostr.split("%22");
-  var chkfltyp = fullurl[1].split(".pdf");
-  if (r == "true" && chkfltyp[1] == "") {
-    $("#crtpdflk,#formContainer").hide();
-    var f_urlF = fullurl[1];
-    var f_url = unescape(f_urlF.replace("+", " "));
-    var f_meta = m;
-    document.title = f_meta + " | MASTROWALL";
-  } else if (r == "true" && chkfltyp.length == 1) {
-    $("#objcont").show();
-    $("#crtpdflk,#formContainer").hide();
-    document.getElementById("objcont").innerHTML =
-      "<object data='" + unescape(chkfltyp[0]) + "' id='objfrm'></object>";
-    var f_meta = m;
-    document.title = f_meta + " | MASTROWALL";
+  function getCookie() {
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      const cookie = ca[i].trim();
+      if (
+        cookie.startsWith("mwallpswstud=") ||
+        cookie.startsWith("mwallpswedud=")
+      ) {
+        return true;
+      }
+    }
+    return false;
   }
-  document.body.addEventListener("click", function () {
-    window.open(f_url, "_blank");
-  });
-  document.addEventListener("adobe_dc_view_sdk.ready", function () {
-    var adobeDCView = new AdobeDC.View({
-      clientId: "cd60999abf224dbabfb9705314ec2056",
-      divId: "adobe-view",
+  if (getCookie() === false) {
+    document.getElementById("crtpdflk").remove();
+  }
+  $(document).ready(function () {
+    $("#crtpdflk").click(function () {
+      createForm();
     });
-    adobeDCView.previewFile(
-      {
-        content: { location: { url: f_url } },
-        metaData: { fileName: f_meta },
-      },
-      {}
-    );
   });
 
   function createForm() {
@@ -82,6 +66,49 @@ document.addEventListener("DOMContentLoaded", function () {
       form.appendChild(input3);
     });
     document.getElementById("formContainer").appendChild(form);
+  }
+
+  var url_string = window.location.href;
+  var url = new URL(url_string);
+  var q = url.searchParams.get("f");
+  var m = url.searchParams.get("m");
+  var r = url.searchParams.get("r");
+  var tostr = url.searchParams.toString();
+  var fullurl = tostr.split("%22");
+  var chkfltyp = fullurl[1] ? fullurl[1].split(".pdf") : [];
+
+  if (r === "true" && chkfltyp[1] === "") {
+    $("#crtpdflk,#formContainer").hide();
+    var f_urlF = fullurl[1];
+    var f_url = unescape(f_urlF.replace(/\+/g, " "));
+    var f_meta = m;
+    document.title = f_meta + " | MASTROWALL";
+
+    document.addEventListener("adobe_dc_view_sdk.ready", function () {
+      var adobeDCView = new AdobeDC.View({
+        clientId: "cd60999abf224dbabfb9705314ec2056",
+        divId: "adobe-view",
+      });
+      adobeDCView.previewFile(
+        {
+          content: { location: { url: f_url } },
+          metaData: { fileName: f_meta },
+        },
+        {}
+      );
+    });
+
+    document.body.addEventListener("click", function () {
+      window.open(f_url, "_blank");
+    });
+  } else if (r === "true" && chkfltyp.length === 1) {
+    $("#objcont").show();
+    $("#crtpdflk,#formContainer").hide();
+    var f_url = unescape(chkfltyp[0].replace(/\+/g, " "));
+    var f_meta = m;
+    document.title = f_meta + " | MASTROWALL";
+    document.getElementById("objcont").innerHTML =
+      "<object data='" + f_url + "' id='objfrm'></object>";
   }
 
   setInterval(function () {
